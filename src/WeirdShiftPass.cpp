aaf34d166faf7296b714f8eccc25f471e28afb51
Neil Mendoza <neil@isthis.gd>
2013-12-07 18:56:57 +0100

    WeirdShiftPass test

---
+++ src/WeirdShiftPass.cpp
@@ -0,0 +1,86 @@
+/*
+ *  WeirdShiftPass.cpp
+ *
+ *  Copyright (c) 2013, satcy, http://satcy.net
+ *  All rights reserved. 
+ *  
+ *  Redistribution and use in source and binary forms, with or without 
+ *  modification, are permitted provided that the following conditions are met: 
+ *  
+ *  * Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer. 
+ *  * Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution. 
+ *  * Neither the name of Neil Mendoza nor the names of its contributors may be used 
+ *    to endorse or promote products derived from this software without 
+ *    specific prior written permission. 
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ *  POSSIBILITY OF SUCH DAMAGE. 
+ *
+ */
+#include "WeirdShiftPass.h"
+
+namespace itg
+{
+    WeirdShiftPass::WeirdShiftPass(const ofVec2f& aspect) :
+        RenderPass(aspect, "weirdshift"), h(2.0/512.0), r(0.5)
+    {
+        string fragShaderSrc = STRINGIFY(
+             uniform sampler2D tDiffuse;
+             uniform float h;
+             uniform float r;
+             
+             void main() {
+                 vec2 vUv = gl_TexCoord[0].st;
+                 vec4 sum = vec4( 0.0 );
+                 
+                 float hh = h * abs( sin(20.0 * vUv.y) );
+                 
+                 sum += texture2D( tDiffuse, vec2( vUv.x - 4.0 * hh, vUv.y ) ) * 0.051;
+                 sum += texture2D( tDiffuse, vec2( vUv.x - 3.0 * hh, vUv.y ) ) * 0.0918;
+                 sum += texture2D( tDiffuse, vec2( vUv.x - 2.0 * hh, vUv.y ) ) * 0.12245;
+                 sum += texture2D( tDiffuse, vec2( vUv.x - 1.0 * hh, vUv.y ) ) * 0.1531;
+                 //sum += texture2D( tDiffuse, vec2( vUv.x, vUv.y ) ) * 0.1633;
+                 sum += texture2D( tDiffuse, vec2( vUv.x + 1.0 * hh, vUv.y ) ) * 0.1531;
+                 sum += texture2D( tDiffuse, vec2( vUv.x + 2.0 * hh, vUv.y ) ) * 0.12245;
+                 sum += texture2D( tDiffuse, vec2( vUv.x + 3.0 * hh, vUv.y ) ) * 0.0918;
+                 sum += texture2D( tDiffuse, vec2( vUv.x + 4.0 * hh, vUv.y ) ) * 0.051;
+                 
+                 gl_FragColor = sum;
+             }
+        );
+        
+        shader.setupShaderFromSource(GL_FRAGMENT_SHADER, fragShaderSrc);
+        shader.linkProgram();
+#ifdef _ITG_TWEAKABLE
+        addParameter("f", this->h, "min=0 max=1");
+        addParameter("r", this->r, "min=0 max=1");
+#endif
+    }
+    
+    void WeirdShiftPass::render(ofFbo& readFbo, ofFbo& writeFbo)
+    {
+        writeFbo.begin();
+        
+        shader.begin();
+        shader.setUniformTexture("tDiffuse", readFbo.getTextureReference(), 0);
+        shader.setUniform1f("h", h);
+        shader.setUniform1f("r", r);
+        
+        texturedQuad(0, 0, writeFbo.getWidth(), writeFbo.getHeight());
+        
+        shader.end();
+        writeFbo.end();
+    }
+}
